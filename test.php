6fbe2e975b259f3af78d353ab6348a6797534d75
Almas Bekbayev <bekmaskz@gmail.com>
2020-06-20 15:56:11 +0600

    Изучаем абстрактные классы в PHP

--- test.php
+++ test.php
@@ -1,26 +1,71 @@
 <?php
 
-class A
+/* Домашнее задание:
+ * 1. Создайте абстрактный класс Human.
+ *
+ * 2. Отнаследуйте от него 2 класса: RussianHuman и EnglishHuman.
+ *
+ * 3. Реализуйте в них методы getGreetings(), которые будут возвращать приветствие на разных языках, вроде «Привет».
+ *
+ * 4. Реализуйте в них методы getMyNameIs(), которые будут возвращать на разных языках слова «Меня зовут».
+ *
+ * 5. В итоге метод introduceYourself должен возвращать строку, вроде «Привет! Меня зовут Иван.»
+ *
+ * 6. Создайте объекты этих классов и заставьте их поздороваться.
+ */
+
+abstract class Human
 {
-    public function method1()
+    private $name;
+
+    public function __construct(string $name)
     {
-        return $this->method2();
+        $this->name = $name;
     }
 
-    protected function method2()
+    public function getName(): string
     {
-        return 'A';
+        return $this->name;
+    }
+
+    abstract public function getGreetings(): string;
+    abstract public function getMyNameIs(): string;
+
+    public function introduceYourself(): string
+    {
+        return $this->getGreetings() . '! ' . $this->getMyNameIs() . ' ' . $this->getName() . '.';
     }
 }
 
-class B extends A
+class RussianHuman extends Human
 {
-    protected function method2()
+    public function getGreetings(): string
     {
-        return 'B';
+        return 'Привет';
+    }
+
+    public function getMyNameIs(): string
+    {
+        return 'Меня зовут';
     }
 }
 
-$b = new B();
+class EnglishHuman extends Human
+{
+    public function getGreetings(): string
+    {
+        return 'Hello';
+    }
 
-echo $b->method1(); // B
+    public function getMyNameIs(): string
+    {
+        return 'My name is';
+    }
+}
+
+$anton = new RussianHuman('Антон');
+$james = new EnglishHuman('James');
+
+echo $anton->introduceYourself();
+echo '<br>';
+echo $james->introduceYourself();
