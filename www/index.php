915f709cb1528ebe568e1f0964f93a8e21cbc273
Almas Bekbayev <bekmaskz@gmail.com>
2020-06-20 17:08:52 +0600

    Controller в MVC

--- www/index.php
+++ www/index.php
@@ -4,6 +4,11 @@ spl_autoload_register(function (string $classname) {
     require_once __DIR__ . '/../src/' . $classname . '.php';
 });
 
-$author = new \MyProject\Models\Users\User('Иван');
-$article = new \MyProject\Models\Articles\Article('Заголовок', 'Текст', $author);
-var_dump($article);
+$controller = new \MyProject\Controllers\MainController();
+
+if (!empty($_GET['name'])) {
+    $controller->sayHello($_GET['name']);
+} else {
+    $controller->main();
+}
+
