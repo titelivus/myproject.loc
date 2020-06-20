d959b9c55bf05a12c36266fd495ee3aea5cb6270
Almas Bekbayev <bekmaskz@gmail.com>
2020-06-20 16:07:51 +0600

    Статические методы и свойства в PHP

--- test.php
+++ test.php
@@ -1,71 +1,22 @@
 <?php
 
-/* Домашнее задание:
- * 1. Создайте абстрактный класс Human.
- *
- * 2. Отнаследуйте от него 2 класса: RussianHuman и EnglishHuman.
- *
- * 3. Реализуйте в них методы getGreetings(), которые будут возвращать приветствие на разных языках, вроде «Привет».
- *
- * 4. Реализуйте в них методы getMyNameIs(), которые будут возвращать на разных языках слова «Меня зовут».
- *
- * 5. В итоге метод introduceYourself должен возвращать строку, вроде «Привет! Меня зовут Иван.»
- *
- * 6. Создайте объекты этих классов и заставьте их поздороваться.
- */
-
-abstract class Human
+class Human
 {
-    private $name;
+    private static $count = 0;
 
-    public function __construct(string $name)
+    public function __construct()
     {
-        $this->name = $name;
+        self::$count++;
     }
 
-    public function getName(): string
+    public static function getCount(): int
     {
-        return $this->name;
-    }
-
-    abstract public function getGreetings(): string;
-    abstract public function getMyNameIs(): string;
-
-    public function introduceYourself(): string
-    {
-        return $this->getGreetings() . '! ' . $this->getMyNameIs() . ' ' . $this->getName() . '.';
+        return self::$count;
     }
 }
 
-class RussianHuman extends Human
-{
-    public function getGreetings(): string
-    {
-        return 'Привет';
-    }
-
-    public function getMyNameIs(): string
-    {
-        return 'Меня зовут';
-    }
+for ($i = rand(3, 15); $i != 0; $i--) {
+    new Human();
 }
 
-class EnglishHuman extends Human
-{
-    public function getGreetings(): string
-    {
-        return 'Hello';
-    }
-
-    public function getMyNameIs(): string
-    {
-        return 'My name is';
-    }
-}
-
-$anton = new RussianHuman('Антон');
-$james = new EnglishHuman('James');
-
-echo $anton->introduceYourself();
-echo '<br>';
-echo $james->introduceYourself();
+echo Human::getCount();
