905c097e1e23f9db4a58f4119eb775eca96f6342
Almas Bekbayev <bekmaskz@gmail.com>
2020-06-21 20:13:21 +0600

    View в MVC

--- src/MyProject/Controllers/MainController.php
+++ src/MyProject/Controllers/MainController.php
@@ -2,11 +2,24 @@
 
 namespace MyProject\Controllers;
 
+use MyProject\View\View;
+
 class MainController
 {
+    private $view;
+
+    public function __construct()
+    {
+        $this->view = new View(__DIR__ . '/../../../templates');
+    }
+
     public function main()
     {
-        echo 'Главная страница';
+        $articles = [
+            ['name' => 'Статья 1', 'text' => 'Текст статьи 1'],
+            ['name' => 'Статья 2', 'text' => 'Текст статьи 2']
+        ];
+        $this->view->renderHtml('main/main.php', ['articles' => $articles]);
     }
 
     public function sayHello(string $name)
