dc238429c366568e88af42e9255c339ee7fc553e
Almas Bekbayev <bekmaskz@gmail.com>
2020-06-21 20:34:18 +0600

    Класс для работы с базой данных

--- src/MyProject/Controllers/MainController.php
+++ src/MyProject/Controllers/MainController.php
@@ -3,23 +3,27 @@
 namespace MyProject\Controllers;
 
 use MyProject\View\View;
+use MyProject\Services\Db;
 
 class MainController
 {
+    /** @var View */
     private $view;
+
+    /** @var Db */
+    private $db;
 
     public function __construct()
     {
         $this->view = new View(__DIR__ . '/../../../templates');
+        $this->db = new Db();
     }
 
     public function main()
     {
-        $articles = [
-            ['name' => 'Статья 1', 'text' => 'Текст статьи 1'],
-            ['name' => 'Статья 2', 'text' => 'Текст статьи 2']
-        ];
-        $this->view->renderHtml('main/main.php', ['articles' => $articles]);
+        $articles = $this->db->query('SELECT * FROM articles');
+        var_dump($articles);
+//        $this->view->renderHtml('main/main.php', ['articles' => $articles]);
     }
 
     public function sayHello(string $name)
