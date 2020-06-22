a66258fe7cf6fc0f7e48333a06b75f0297181065
Almas Bekbayev <bekmaskz@gmail.com>
2020-06-22 22:03:23 +0600

    Делаем вывод статей на сайте из базы данных

--- src/MyProject/Controllers/MainController.php
+++ src/MyProject/Controllers/MainController.php
@@ -22,8 +22,7 @@ class MainController
     public function main()
     {
         $articles = $this->db->query('SELECT * FROM articles');
-        var_dump($articles);
-//        $this->view->renderHtml('main/main.php', ['articles' => $articles]);
+        $this->view->renderHtml('main/main.php', ['articles' => $articles]);
     }
 
     public function sayHello(string $name)
--- src/routes.php
+++ src/routes.php
@@ -6,7 +6,6 @@
  */
 
 return [
-    '~^hello/(.*)$~' => [\MyProject\Controllers\MainController::class, 'sayHello'],
-    '~^$~' => [\MyProject\Controllers\MainController::class, 'main'],
-    '~^bye/(.*)$~' => [\MyProject\Controllers\MainController::class, 'sayBye']
+    '~^articles/(\d+)$~' => [\MyProject\Controllers\ArticlesController::class, 'view'],
+    '~^$~' => [\MyProject\Controllers\MainController::class, 'main']
 ];
