f81cadffa4ff7937b70a21f116b4db533fef8e16
Almas Bekbayev <bekmaskz@gmail.com>
2020-06-23 22:08:24 +0600

    Паттерн Singleton

--- src/MyProject/Models/ActiveRecordEntity.php
+++ src/MyProject/Models/ActiveRecordEntity.php
@@ -19,7 +19,7 @@ abstract class ActiveRecordEntity
 
     public static function getById(int $id): ?self
     {
-        $db = new Db();
+        $db = Db::getInstance();
         $entities = $db->query(
             'SELECT * FROM `' . static::getTableName() . '` WHERE id=:id',
             ['id' => $id],
@@ -44,7 +44,7 @@ abstract class ActiveRecordEntity
      */
     public static function findAll(): array
     {
-        $db = new Db();
+        $db = Db::getInstance();
         return $db->query('SELECT * FROM `' . static::getTableName() . '`;', [], static::class);
     }
 
